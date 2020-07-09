15dc537204824bb4d90415ece9308d01de2eef0a
Nikhil Mehta <nikhil@meraklistechnologies.com>
2020-07-09 18:27:40 +0530

    Using DB_URL for now

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Set database config from Heroku DATABASE_URL
-if [ "$DATABASE_URL" != "" ]; then
-    echo "Found database configuration in DATABASE_URL=$DATABASE_URL"
+# Set database config from Heroku DB_URL
+if [ "$DB_URL" != "" ]; then
+    echo "Found database configuration in DB_URL=$DB_URL"
 
     regex='^postgres://([a-zA-Z0-9_-]+):([a-zA-Z0-9]+)@([a-z0-9.-]+):([[:digit:]]+)/([a-zA-Z0-9_-]+)$'
-    if [[ $DATABASE_URL =~ $regex ]]; then
+    if [[ $DB_URL =~ $regex ]]; then
         export DB_ADDR=${BASH_REMATCH[3]}
         export DB_PORT=${BASH_REMATCH[4]}
         export DB_DATABASE=${BASH_REMATCH[5]}
